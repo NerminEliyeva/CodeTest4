5eb3b55514b1da17df581030636247c8d9efd495
NerminEliyeva <narminahmedova1997@gmail.com>
2023-12-07 10:53:34 +0400

    some changes to the code base : -2/-02-10-53-34

--- commit.bat
+++ commit.bat
@@ -1,8 +1,15 @@
-@eco off 
+@eco off
 setlocal
-set MMDDYY=%DATE:~4,2%%DATE:~7,2%%DATE:~12,2%
+
+set "year=%date:~10,4%"
+set "month=%date:~4,2%"
+set "day=%date:~7,2%"
+set "hour=%time:~0,2%"
+set "minute=%time:~3,2%"
+set "second=%time:~6,2%"
+set "datestamp=%year%-%month%-%day%-%hour%-%minute%-%second%"
 
 git add .
-git commit -m "changes from bat : %DATE%"
+git commit -m "some changes to the code base : %datestamp%"
 git push 
 pause
