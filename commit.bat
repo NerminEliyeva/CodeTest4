4f6c88e6b6b43b4f328f027aa60b8421082eadcb
NerminEliyeva <narminahmedova1997@gmail.com>
2023-12-07 11:11:56 +0400

    some changes to the code base : 2023_12_07__11_11_56

--- commit.bat
+++ commit.bat
@@ -1,14 +1,8 @@
 @eco off
 setlocal
 
-set "year=%date:~10,4%"
-set "month=%date:~4,2%"
-set "day=%date:~7,2%"
-set "hour=%time:~0,2%"
-set "minute=%time:~3,2%"
-set "second=%time:~6,2%"
-set "datestamp=%year%-%month%-%day%-%hour%-%minute%-%second%"
-
+set datestamp=%DATE:~6,4%_%DATE:~3,2%_%DATE:~0,2%__%TIME:~0,2%_%TIME:~3,2%_%TIME:~6,2%
+set datestamp=%datestamp: =0%
 
 git add .
 git commit -m "some changes to the code base : %datestamp%"
